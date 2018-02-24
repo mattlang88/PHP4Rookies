919a5120d7909eb2af4482c26364051040392432
mattlang88 <matthias.p.lang@googlemail.com>
2018-02-24 21:33:40 +0100

    Added automatic removal of container after exit to avoid manual removal before starting the container.

--- run-server.sh
+++ run-server.sh
@@ -2,5 +2,6 @@ docker run -d \
     --name training-rookie-guestbook \
     -v $(pwd):/guestbook \
     -p 8811:80 \
+    --rm \
     hub.babydev.de/training/rookie-guestbook:php \
     php -S 0.0.0.0:80 /guestbook/index.php
